04c51f4ab5674d4c137674baa5c7756bb1c198e1
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-27 13:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//your JS code here. If required.
+function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
         const board = document.getElementById("board");
         for (let i = 0; i < 800; i++) {
             const square = document.createElement("div");
             square.classList.add("square");
             square.addEventListener("mouseover", () => {
-                square.style.backgroundColor = "#3498db";
+                const randomColor = getRandomColor();
+                square.style.backgroundColor = randomColor;
                 setTimeout(() => {
                     square.style.backgroundColor = "#ddd";
                 }, 1000);
