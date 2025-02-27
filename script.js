b78c31015271f80783404bd559e19d4025dbe49e
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-27 13:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+        const board = document.getElementById("board");
+        for (let i = 0; i < 800; i++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseover", () => {
+                square.style.backgroundColor = "#3498db";
+                setTimeout(() => {
+                    square.style.backgroundColor = "#ddd";
+                }, 1000);
+            });
+            board.appendChild(square);
+        }
